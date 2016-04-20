271bd8bc0007d88e46660cc2a3f7b99c64d727f4
Naeem Talukdar <naeem.talukdar@gmail.com>
2016-04-20 11:36:43 -0400

    New animations and new base screen

--- MainScreen.android.js
+++ MainScreen.android.js
@@ -7,6 +7,8 @@ import React, {
   Text
 } from 'react-native'
 
+import Animatable from 'react-native-animatable'
+
 import {
   registerListener
 } from './api/BeaconsApi.js'
@@ -42,29 +44,99 @@ export default class MainScreen extends Component {
 
     var toggl = true
 
+    const styles = StyleSheet.create({
+      triggerBackground: {
+        borderColor: this.state.borderColor || 'red',
+        borderWidth: 2.5,
+        height: 225,
+        borderRadius: 225,
+        width: 225,
+        padding: 10
+      },
+      report: {
+        flexDirection:'row',
+        marginBottom: 15,
+      },
+      reportIcon: {
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 25,
+        padding: 10
+      },
+      trigger: {
+        justifyContent: 'center',
+        padding: 10,
+        alignItems: 'center',
+        backgroundColor: 'rgba(250,250,250,0.15)',
+        borderRadius: 200,
+        height: 200,
+        width: 200,
+      },
+      triggerText: {
+        color: 'white',
+        fontFamily: 'Helvetica Neue Light',
+        fontSize: 48
+      },
+      reportLabel: {
+        textAlign: 'center',
+        color: 'white',
+        fontFamily: 'Helvetica Neue Light',
+        fontSize: 16,
+        marginBottom: 40
+      },
+      reportTimer: {
+        alignItems: 'center',
+        flexDirection:'row',
+        color: 'white',
+        fontFamily: 'Helvetica Neue Light',
+        fontSize: 12,
+        marginTop: 15
+      }
+    });
+
+
     return (
       <View>
       {toggl &&
-        <View style={styles.triggerBackground}>
-            <TouchableOpacity activeOpacity={0.6} style={styles.trigger}>
-              <Text style={styles.triggerText}>
+        <Animatable.View ref="view" transition='borderColor' easing='ease-out' style={styles.triggerBackground} >
+            <TouchableOpacity activeOpacity={0.6} style={styles.trigger} onPress={() => this.refs.view.bounce(800).then((endState) => this.setState({borderColor: this.state.borderColor == 'green' ? 'red' : 'green'})) }>
+              <Animatable.Text style={styles.triggerText}>
                 00:01
-              </Text>
+              </Animatable.Text>
             </TouchableOpacity>      
-        </View>
+        </Animatable.View>
       }
       {!toggl &&
         <View>
           <Text style={styles.reportLabel}>
             APRIL, 25. 2016
           </Text>  
-          <View style={styles.report}>
-            <View style={styles.reportIcon}>
-              <Image
-                source={require('./assets/images/Dumbbell-50.png')}
-              />
+          <View>
+            <View style={styles.report}>
+              <View style={styles.reportIcon}>
+                <Image
+                  source={require('./assets/images/Dumbbell-50.png')}
+                />
+              </View>
+              <View style={{ height: 10, borderRadius: 5, backgroundColor: '#04C5A6', marginTop: 20, marginLeft: 15, marginRight: 15, width: 200 }}>
+              </View>
+              <Text style={styles.reportTimer}>
+                20:45
+              </Text>
             </View>
-            <View style={styles.progressBar}>
+          </View>
+          <View>
+            <View style={styles.report}>
+              <View style={styles.reportIcon}>
+                <Image
+                  source={require('./assets/images/Dumbbell-50.png')}
+                />
+              </View>
+              <View style={{ height: 10, borderRadius: 5, backgroundColor: '#04C5A6', marginTop: 20, marginLeft: 15, marginRight: 15, width: 100 }}>
+              </View>
+              <Text style={styles.reportTimer}>
+                13:45
+              </Text>
             </View>
           </View>
         </View>
@@ -73,53 +145,3 @@ export default class MainScreen extends Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  triggerBackground: {
-    borderColor: 'green',
-    borderWidth: 2.5,
-    height: 225,
-    borderRadius: 225,
-    width: 225,
-    padding: 10
-  },
-  report: {
-    flexDirection:'row',
-    marginTop: 40
-  },
-  reportIcon: {
-    borderWidth: 1,
-    borderColor: 'white',
-    borderRadius: 25,
-    padding: 10
-  },
-  progressBar: {
-    height: 10,
-    width: 150,
-    borderRadius: 5,
-    backgroundColor: '#04C5A6',
-    marginTop: 20,
-    marginLeft: 15
-  },
-  trigger: {
-    justifyContent: 'center',
-    padding: 10,
-    alignItems: 'center',
-    backgroundColor: 'rgba(250,250,250,0.15)',
-    borderRadius: 200,
-    height: 200,
-    width: 200,
-  },
-  triggerText: {
-    color: 'white',
-    fontFamily: 'Helvetica Neue Light',
-    fontSize: 48
-  },
-  reportLabel: {
-    alignItems: 'center',
-    flexDirection:'row',
-    color: 'white',
-    fontFamily: 'Helvetica Neue Light',
-    fontSize: 16
-  }
-});
